45f3f1aa763e953a8bef3e7ac5ad5078e2461f52
GONI Guillaume <gdn3@live.fr>
2017-08-08 11:37:06 +0200

    deleted _references.js (deprecated) + slide to fill screen + next sldie js

--- js/_references.js
+++
@@ -1,4 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="jquery-3.2.1.js" />
-/// <reference path="bootstrap.js" />
-/// <reference path="app.js" />
--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
 var main = function() {
-    $("down-arrow button").click(function() {
-        $("htlm, body").animate({'scrolltop': findNextRow(this)}, 500);
-    })
+    //$(".down-arrow button").click(scrollToNextSlide);
+    $(document).on("click",".down-arrow button",scrollToNextSlide);
 }
 
-var findNextRow = function(elm) {
-    while(! elm.hasClass("row"))
-        elm = elm.parent();
+var getCurrentSlideIndex = function() {
+    const slidesOffsets = $(".slide").map(function(index, elm) {return $(elm).offset().top});
+    const currentScroll = $("html,body").scrollTop();
+    var i = 0;
 
-    return elm.next();
+    for(i = slidesOffsets.length - 1 ; i > 0 && currentScroll < slidesOffsets[i] ; i--) {}
+
+    return i;
+}
+
+var getSlideTopOffset = function(slideIndex) {
+    var slides = $(".slide");
+    if (slideIndex < 0)
+        slideIndex = 0;
+    else if (slideIndex >= slides.length)
+        slideIndex = slides.length -1;
+
+    return $(slides[slideIndex]).offset().top;
+}
+
+var scrollToPreviousSlide = function() {
+    $("html, body").animate({'scrollTop': getSlideTopOffset(getCurrentSlideIndex()-1)}, 500);
+}
+var scrollToNextSlide = function() {
+    $("html, body").animate({'scrollTop': getSlideTopOffset(getCurrentSlideIndex()+1)}, 500);
 }
 
 $(document).ready(main);
