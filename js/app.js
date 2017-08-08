06bf7fbeaf6dd906364013235401eb757249ceb3
GONI Guillaume <gdn3@live.fr>
2017-08-08 15:45:43 +0200

    eslint conf + formated code + right side nav bar + dl btn

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
-var main = function() {
+var main = function () {
     $(".arrow").removeClass("hidden")
     $(".up-arrow button").click(scrollToPreviousSlide);
     $(".down-arrow button").click(scrollToNextSlide);
 }
 
-var getCurrentSlideIndex = function() {
-    const slidesOffsets = $(".slide").map(function(index, elm) {return $(elm).offset().top});
+var getCurrentSlideIndex = function () {
+    const slidesOffsets = $(".slide").map(function (index, elm) { return $(elm).offset().top });
     const currentScroll = $("html,body").scrollTop();
     var i = 0;
 
-    for(i = slidesOffsets.length - 1 ; i > 0 && currentScroll < slidesOffsets[i] ; i--) {}
+    for (i = slidesOffsets.length - 1; i > 0 && currentScroll < slidesOffsets[i]; i--) { }
 
     return i;
 }
 
-var getSlideTopOffset = function(slideIndex) {
+var getSlideTopOffset = function (slideIndex) {
     var slides = $(".slide");
     if (slideIndex < 0)
         slideIndex = 0;
     else if (slideIndex >= slides.length)
-        slideIndex = slides.length -1;
+        slideIndex = slides.length - 1;
 
     return $(slides[slideIndex]).offset().top;
 }
 
-var scrollToPreviousSlide = function() {
-    $("html, body").animate({'scrollTop': getSlideTopOffset(getCurrentSlideIndex()-1)}, 500);
+var scrollToPreviousSlide = function () {
+    $("html, body").animate({ 'scrollTop': getSlideTopOffset(getCurrentSlideIndex() - 1) }, 500);
 }
-var scrollToNextSlide = function() {
-    $("html, body").animate({'scrollTop': getSlideTopOffset(getCurrentSlideIndex()+1)}, 500);
+var scrollToNextSlide = function () {
+    $("html, body").animate({ 'scrollTop': getSlideTopOffset(getCurrentSlideIndex() + 1) }, 500);
 }
 
 $(document).ready(main);
