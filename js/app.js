ec5303e4821d2fef1fea3f59a82f7f7f0fc815b4
GONI Guillaume <gdn3@live.fr>
2017-08-23 13:50:07 +0200

    hreflang links + enclosed js code in func

--- js/app.js
+++ js/app.js
@@ -1,69 +1,71 @@
-const main = function () {
-    $(".up-arrow button").click(scrollToPreviousSlide);
-    $(".down-arrow button").click(scrollToNextSlide);
+(function () {
+    const main = function () {
+        $(".up-arrow button").click(scrollToPreviousSlide);
+        $(".down-arrow button").click(scrollToNextSlide);
 
-    $(window).scroll(hideArrowsOnScroll);
+        $(window).scroll(hideArrowsOnScroll);
 
-    hideArrowsOnScroll();
+        hideArrowsOnScroll();
 
-    $("html").keydown(function (t) {
-        switch (t.which) {
-            case 38:
-                scrollToPreviousSlide();
-                break;
-            case 32:
-            case 40:
-                scrollToNextSlide();
-                break;
-            default:
-                break;
-        }
-    })
-}
+        $("html").keydown(function (t) {
+            switch (t.which) {
+                case 38:
+                    scrollToPreviousSlide();
+                    break;
+                case 32:
+                case 40:
+                    scrollToNextSlide();
+                    break;
+                default:
+                    break;
+            }
+        })
+    }
 
-const getCurrentSlideIndex = function () {
-    const slidesOffsets = $(".slide").map(function (index, elm) { return $(elm).offset().top });
-    const currentScroll = $(window).scrollTop() + $(window).height() / 2;
-    let i = 0;
+    const getCurrentSlideIndex = function () {
+        const slidesOffsets = $(".slide").map(function (index, elm) { return $(elm).offset().top; });
+        const currentScroll = $(window).scrollTop() + $(window).height() / 2;
+        let i = 0;
 
-    for (i = slidesOffsets.length - 1; i > 0 && currentScroll < slidesOffsets[i]; i--);
+        for (i = slidesOffsets.length - 1; i > 0 && currentScroll < slidesOffsets[i]; i--);
 
-    return i;
-}
+        return i;
+    }
 
-const getSlideTopOffset = function (slideIndex) {
-    const slides = $(".slide");
-    if (slideIndex < 0)
-        slideIndex = 0;
-    else if (slideIndex >= slides.length)
-        slideIndex = slides.length - 1;
+    const getSlideTopOffset = function (slideIndex) {
+        const slides = $(".slide");
+        if (slideIndex < 0)
+            slideIndex = 0;
+        else if (slideIndex >= slides.length)
+            slideIndex = slides.length - 1;
 
-    return $(slides[slideIndex]).offset().top;
-}
+        return $(slides[slideIndex]).offset().top;
+    }
 
-const scrollToPreviousSlide = function () {
-    $("html, body").animate({ 'scrollTop': getSlideTopOffset(getCurrentSlideIndex() - 1) }, 500);
-}
-const scrollToNextSlide = function () {
-    $("html, body").animate({ 'scrollTop': getSlideTopOffset(getCurrentSlideIndex() + 1) }, 500);
-}
+    const scrollToPreviousSlide = function () {
+        $("html, body").animate({ 'scrollTop': getSlideTopOffset(getCurrentSlideIndex() - 1) }, 500);
+    }
+    const scrollToNextSlide = function () {
+        $("html, body").animate({ 'scrollTop': getSlideTopOffset(getCurrentSlideIndex() + 1) }, 500);
+    }
 
-const hideArrowsOnScroll = function () {
-    const scrollTop = $(window).scrollTop();
-    const navLinks = $("#navbar-links li");
+    const hideArrowsOnScroll = function () {
+        const scrollTop = $(window).scrollTop();
+        const navLinks = $("#navbar-links li");
 
-    if (scrollTop <= getSlideTopOffset(0))
-        $(".up-arrow").addClass("hidden")
-    else
-        $(".up-arrow").removeClass("hidden")
+        if (scrollTop <= getSlideTopOffset(0))
+            $(".up-arrow").addClass("hidden")
+        else
+            $(".up-arrow").removeClass("hidden")
 
-    if (scrollTop >= getSlideTopOffset($(".slide").length - 1))
-        $(".down-arrow").addClass("hidden")
-    else
-        $(".down-arrow").removeClass("hidden")
-    
-    navLinks.removeClass("active");
-    $(navLinks[getCurrentSlideIndex()]).addClass("active");
-}
+        if (scrollTop >= getSlideTopOffset($(".slide").length - 1))
+            $(".down-arrow").addClass("hidden")
+        else
+            $(".down-arrow").removeClass("hidden")
 
-$(document).ready(main);
+        navLinks.removeClass("active");
+        $(navLinks[getCurrentSlideIndex()]).addClass("active");
+    }
+
+    $(document).ready(main);
+})()
